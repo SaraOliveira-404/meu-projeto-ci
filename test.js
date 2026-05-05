d9f98b1b34cd680f7f26dd43496b9690053c00f5
SaraOliveira-404 <saraoliveira.academico@gmail.com>
2026-05-05 13:33:48 -0300

    test.js

--- test.js
+++ test.js
@@ -1,10 +1,21 @@
 const saudacao = require('./hello');
+
 // Teste com erro intencional
 const resultado = saudacao("Mundo");
-const esperado = "Hello, Mundo!"; // Erro proposital! ⚠️
+//const esperado = "Hello, Mundo!"; // Erro proposital!
+const esperado = "Olá, Mundo!"; // Erro proposital corrigido
+
 if (resultado !== esperado) {
   console.error(` FALHA: esperado "${esperado}", mas
 recebeu "${resultado}"`);
   process.exit(1); // Faz o CI falhar
 }
+
 console.log(" Todos os testes passaram!");
+
+// Teste para nome vazio
+const resultadoVazio = saudacao("");
+if (resultadoVazio !== "Olá, !") {
+  console.error(` FALHA: teste de string vazia`);
+  process.exit(1);
+}
